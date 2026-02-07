f24a0160f64c9548ee0ae50ba7b28dd304b9b9c9
Ledger Dev <dev@ledger.com>
2026-02-07 19:05:00 +0100

    fix: [LE-73] Bind API to 0.0.0.0 and wait for DB

--- ledger-api/src/main.ts
+++ ledger-api/src/main.ts
@@ -6,10 +6,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: '*'
-    }
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
   });
 
   const config = new DocumentBuilder()
@@ -29,7 +28,10 @@ async function bootstrap() {
   }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
-
+  /*
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+  */
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
